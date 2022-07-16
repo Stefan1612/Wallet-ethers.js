491c1a08575218ce47102b82e90ea50bc20dacc1
Stefan Lehmann <85541624+Stefan1612@users.noreply.github.com>
2022-07-16 20:42:35 +0200

    Create transfering Ethers.js

---
+++ transfering Ethers.js
@@ -0,0 +1,37 @@
+import { ethers } from "ethers";
+
+// setting provider
+
+window.ethersProvider = new ethers.providers.InfuraProvider("ropsten")
+
+// setting wallet
+let wallet = new ethers.Wallet(private_key)
+// connect wallet to net
+let walletSigner = wallet.connect(window.ethersProvider)
+
+// or use getSigner
+let signer = provider.getSigner();
+
+// get current gas price
+window.ethersProvider.getGasPrice()
+
+const tx = {
+  from: send_account,
+  to: to_address,
+  value: ethers.utils.parseEther(send_token_amount),
+  nonce: window.ethersProvider.getTransactionCount(send_account, "latest"),
+  gasLimit: ethers.utils.hexlify(gas_limit), // 100000
+  gasPrice: gas_price,
+}
+
+// either
+walletSigner.sendTransaction(tx).then((transaction) => {
+  console.dir(transaction)
+  alert("Send finished!")
+})
+
+// or
+signer.sendTransaction(tx).then((transaction) => {
+  console.dir(transaction)
+  alert("Send finished!")
+})
